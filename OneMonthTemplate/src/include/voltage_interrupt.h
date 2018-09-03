b1ccb37860d0b665f0363a4836c965166a7135d7
Sean Widmier <seanwidmier@gmail.com>
2018-09-03 14:42:37 -0500

    add voltage_interrupt.h

---
+++ OneMonthTemplate/src/include/voltage_interrupt.h
@@ -0,0 +1,15 @@
+/*
+ * voltage_interrupt.h
+ *
+ * Created: 9/3/2018 2:33:34 PM
+ *  Author: Sean
+ */ 
+
+
+#ifndef VOLTAGE_INTERRUPT_H_
+#define VOLTAGE_INTERRUPT_H_
+
+void initialize_interrupt(void);
+
+
+#endif /* VOLTAGE_INTERRUPT_H_ */
