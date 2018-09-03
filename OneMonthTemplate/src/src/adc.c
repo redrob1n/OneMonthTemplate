bec508d85bf82fbe2b474cd31bfe3e6eedbd16a4
Sean Widmier <seanwidmier@gmail.com>
2018-09-03 14:28:38 -0500

    adc code

---
+++ OneMonthTemplate/src/src/adc.c
@@ -0,0 +1,23 @@
+/*
+ * adc.c
+ *
+ * Created: 9/3/2018 2:11:20 PM
+ *  Author: Sean
+ */ 
+void adc_init(void)
+{
+	ADCA.CTRLA = 0x01;			// Enables the ADC
+	ADCA.CTRLB = 0x00;				// Unsigned 12 bit mode
+	ADCA.REFCTRL = 0x10;			// Voltage Refreence of Vcc/1.6V
+	ADCA.PRESCALER = 0x05;			// This is an automatic prescaler of 128 on the clock
+	
+	ADCA.CH0.CTRL = 0x01;			// Single ended input
+	ADCA.CH0.MUXCTRL = 0x00;		// Reading ADA pin 0	
+}
+
+uint16_t adc_read(void)
+{
+	ADCA.CH0.CTRL |= 0x80;			// Start the conversion
+	while(!(ADCA.CH0.INTFLAGS));	// Wait until the conversion is done
+	return ADCA.CH0.RES;			// Return the 12-bit result as a uint16_t
+}
---
+++ OneMonthTemplate/src/src/adc.h
@@ -0,0 +1,15 @@
+/*
+ * adc.h
+ *
+ * Created: 9/3/2018 2:11:29 PM
+ *  Author: Sean
+ */ 
+
+
+#ifndef ADC_H_
+#define ADC_H_
+
+void adc_init(void);
+uint16_t adc_read(void);
+
+#endif /* ADC_H_ */
