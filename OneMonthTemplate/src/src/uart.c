47ae453d3374e4f5ce43d69b578059119cddf736
Sean Widmier <seanwidmier@gmail.com>
2018-09-03 12:41:24 -0500

    add uart

---
+++ OneMonthTemplate/src/src/uart.c
@@ -0,0 +1,26 @@
+/*
+ * uart.c
+ *
+ * Created: 9/3/2018 12:22:41 PM
+ *  Author: Sean
+ */ 
+
+#include "uart.h"
+
+void uart_terminal_init()
+{
+	sysclk_enable_peripheral_clock(UART_TERMINAL_SERIAL);	//enable the USART's clock
+	//initialize a configuration struct with USART settings
+	static usart_serial_options_t usart_config = {
+		.baudrate	=	UART_TERMINAL_SERIAL_BAUDRATE,
+		.charlength =	UART_TERMINAL_SERIAL_CHAR_LEN,
+		.paritytype =	UART_TERMINAL_SERIAL_PARITY,
+		.stopbits	=	UART_TERMINAL_SERIAL_STOP_BIT
+	};
+	
+	PORTD.DIR |= UART_TERMINAL_TX_PIN;	//set the USART transmit pin to output
+	
+	stdio_serial_init(UART_TERMINAL_SERIAL, &usart_config); //function maps the serial output to printf, not nessecary to know how it works
+	
+	delay_us(1); //apparently need to slow down before print @32MHz
+}
